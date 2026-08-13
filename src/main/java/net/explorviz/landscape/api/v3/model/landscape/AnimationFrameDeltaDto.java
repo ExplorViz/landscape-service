0eb058ea372f8976484cb0e9e5b908e6f95e3f27
UnknownLJackson <daniel.paul.riese@gmail.com>
2026-08-14 00:00:00 +0200

    Change 1 Implemented Commit/Time Based grouping, Change 2 Implemented FileHistory, Change 3 Rewrote backend for actually only Delta changes to improve performance. Old Version Still in Code and able to toggle for compariision reasons

--- src/main/java/net/explorviz/landscape/api/v3/model/landscape/AnimationFrameDeltaDto.java
+++ src/main/java/net/explorviz/landscape/api/v3/model/landscape/AnimationFrameDeltaDto.java
@@ -4,7 +4,7 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.List;
 import java.util.Objects;
 
-/** Keyframes carry the full present {@code state} plus the {@code changes} of that frame */
+/** Keyframes carry the full present {@code state} plus the {@code changes} of that frame. */
 @RegisterForReflection
 public record AnimationFrameDeltaDto(
     String commitHash,
